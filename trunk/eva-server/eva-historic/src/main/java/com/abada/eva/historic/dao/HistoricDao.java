378e629ef5f6e9b0fbe850418f0e4f142e626876
miguel <mamartin@abadasoft.com>
2012-07-25 10:41:20 +0200

    Create eva-historic

---
+++ trunk/eva-server/eva-historic/src/main/java/com/abada/eva/historic/dao/HistoricDao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.eva.historic.dao;
+
+/**
+ *
+ * @author mmartin
+ */
+public interface HistoricDao {
+    
+}
---
+++ trunk/eva-server/eva-historic/src/main/java/com/abada/eva/historic/dao/impl/HistoricDaoImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.eva.historic.dao.impl;
+
+import com.abada.eva.historic.dao.HistoricDao;
+
+/**
+ *
+ * @author mmartin
+ */
+public class HistoricDaoImpl implements HistoricDao{
+    
+    
+}
