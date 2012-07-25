55396ecd4adcb48f402cf450f5815234c131594a
jesus <erchurla@gmail.com>
2012-07-25 11:00:51 +0200

    add logic historic event to HL7 controller and spring configuration

--- trunk/eva-server/eva-rest/src/main/java/com/abada/eva/rest/HL7Controller.java
+++ trunk/eva-server/eva-rest/src/main/java/com/abada/eva/rest/HL7Controller.java
@@ -4,6 +4,9 @@
  */
 package com.abada.eva.rest;
 
+import ca.uhn.hl7v2.model.Message;
+import com.abada.eva.historic.service.HistoricEventService;
+import com.abada.eva.hl7.service.HL7Service;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
@@ -18,17 +21,38 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HL7Controller {
     
-    
+    @Resource(name = "historicEventService")
+    private HistoricEventService heservice;
+    @Resource(name = "hl7Service")
+    private HL7Service hl7service;
     
     @RequestMapping(value = "/rs/sendmessage", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView send(String hl7, HttpServletRequest request){
+    public ModelAndView send(String hl7, HttpServletRequest request) throws Exception{
         
-        @Resource(name = "historicEventService")
-        private HistoricEventService heservice;
+        Message msg = hl7service.buildMessage(hl7);
+        
+        heservice.registerInput(msg, request.getUserPrincipal().getName(), hl7, System.currentTimeMillis());
+        
         
         ModelAndView mav = new ModelAndView("plain");
         mav.addObject("text", "holamundo");
         return mav;
+    }
+    
+    public HistoricEventService getHeservice() {
+        return heservice;
+    }
+
+    public void setHeservice(HistoricEventService heservice) {
+        this.heservice = heservice;
+    }
+
+    public HL7Service getHl7service() {
+        return hl7service;
+    }
+
+    public void setHl7service(HL7Service hl7service) {
+        this.hl7service = hl7service;
     }
     
     
