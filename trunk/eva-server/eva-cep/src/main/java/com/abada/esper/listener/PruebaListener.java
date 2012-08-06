6375ddd612dbca5ec8707ae8b2e2dd8f1558d09d
miguel <mamartin@abadasoft.com>
2012-08-06 14:18:52 +0200

    Create PruebaListener

--- trunk/eva-server/eva-cep/src/main/java/com/abada/esper/listener/PruebaListener.java
+++ trunk/eva-server/eva-cep/src/main/java/com/abada/esper/listener/PruebaListener.java
@@ -11,13 +11,25 @@ import com.espertech.esper.client.UpdateListener;
  *
  * @author mmartin
  */
-public class PruebaListener implements UpdateListener{
+public class PruebaListener implements UpdateListener {
 
     public void update(EventBean[] ebs, EventBean[] ebs1) {
-        
-        System.out.println("Macho yooooooo!!!!!");
-        System.out.println(ebs[0].get("n"));
-        System.out.println("Macho los demas!!!");
+
+        if (ebs != null) {
+            System.out.println("Eventos nuevos:");
+            for(int i=0 ; i<ebs.length ; i++){
+                System.out.println(ebs[i].get("n"));
+                System.out.println("-------------------------------\n\n\n");
+            }
+            
+        } 
+        if (ebs1 != null) {
+            System.out.println("Eventos viejos");
+            for(int i=0 ; i<ebs1.length ; i++){
+                System.out.println(ebs1[i].get("n"));
+                System.out.println("-------------------------------\n\n\n");
+            }
+        }
+        System.out.println("-------------------------------\n\n\n");
     }
-    
 }
