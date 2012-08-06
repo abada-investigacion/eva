7ac24c19eeabac2c3ad150226395f8d628a4f919
jesus <erchurla@gmail.com>
2012-08-06 13:52:42 +0200

    add lock

---
+++ trunk/eva-server/eva-cep/src/main/java/com/abada/esper/lock/dao/LockDaoImpl.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.esper.lock.dao;
+
+
+import com.abada.esper.lock.entities.Lock;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author jesus
+ */
+
+public class LockDaoImpl implements LockDao{
+
+    @PersistenceContext(unitName = "eva-LockPU")
+    private EntityManager entityManager;
+    
+    @Transactional(value = "evaLockService-txm",readOnly=true)
+    public Lock getLastLock() {
+        Query q = entityManager.createQuery("select l from Lock as l where l.id =(select max(lock.id) from Lock as lock)");
+        
+        List<Lock> l= q.getResultList();
+        
+        if(l.size() > 0) return l.get(0);
+        
+        return null;
+    }
+    @Transactional(value = "evaLockService-txm", rollbackFor = {Exception.class})
+    public Lock addLock(Lock lock) {
+        
+        entityManager.persist(lock);
+        
+        return lock;
+    }
+    @Transactional(value = "evaLockService-txm", rollbackFor = {Exception.class})
+    public Lock openLock(Long lockid) {
+        Lock lock = entityManager.find(Lock.class, lockid);
+        lock.setLocked(false);
+        lock.setEnd(System.currentTimeMillis());
+        
+        return lock;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    
+}
