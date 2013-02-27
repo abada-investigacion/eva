64c7c8e5e8bd8e38df82f0185ff78851c65fe5b4
miguel <mamartin@abadasoft.com>
2013-02-27 13:28:06 +0100

    Create epl test

---
+++ trunk/eva-server/eva-listener-test/src/test/java/com/abada/evalistenertest/EventoA.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.evalistenertest;
+
+/**
+ *
+ * @author mmartin
+ */
+public class EventoA {
+    
+    private int id;
+    private String code;
+    private String value;
+
+    public EventoA(int id, String code, String value) {
+        this.id = id;
+        this.code = code;
+        this.value = value;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    
+    
+}
---
+++ trunk/eva-server/eva-listener-test/src/test/java/com/abada/evalistenertest/Listener.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.evalistenertest;
+
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+/**
+ *
+ * @author mmartin
+ */
+public class Listener implements UpdateListener {
+
+    int count;
+
+    public void update(EventBean[] ebs, EventBean[] ebs1) {
+
+        System.out.println("Evento recibido");
+        if (ebs != null) {
+           System.out.println("eventos nuevos: "); 
+            for (EventBean e : ebs) {
+                System.out.println("       "+((EventoA)e.get("a")).getCode() + " " + ((EventoB)e.get("b")).getCode());
+                  
+            }
+        }
+        if (ebs1 != null) {
+        System.out.println("ventos viejos");    
+            for (EventBean e : ebs1) {
+                System.out.println("       "+((EventoA)e.get("a")).getCode() + " " + ((EventoB)e.get("b")).getCode());
+            }
+        }
+
+
+
+    }
+}
