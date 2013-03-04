4ab6110bcd8e5fa67e0fbdadc21974a4570445c6
Katsu <jpelaez@abadasoft.com>
2013-03-04 12:13:13 +0100

    * Add Update Listener for Dimex in Esper

---
+++ trunk/eva-server/eva-actions/src/main/java/com/abada/eva/action/dimex/AbstractDimexUpdateListener.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.abada.eva.action.dimex;
+
+import ca.uhn.hl7v2.model.Message;
+import com.abada.springframework.web.client.RestTemplate;
+import com.abada.springframework.web.client.RestTemplateFactory;
+import com.espertech.esper.client.UpdateListener;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author katsu
+ */
+public abstract class AbstractDimexUpdateListener<T> implements UpdateListener {
+
+    private static final Log logger = LogFactory.getLog(AbstractDimexUpdateListener.class);
+    /**
+     * List of actions to execute
+     */
+    private List<DimexAction> actions;
+    private RestTemplateFactory templateFactory;
+    private String user;
+    private String password;
+    private Class<?> tClass;
+
+    public AbstractDimexUpdateListener() {
+        ParameterizedType superclass = (ParameterizedType) getClass().getGenericSuperclass();
+        this.tClass = (Class<?>) ((ParameterizedType) superclass).getActualTypeArguments()[0];
+    }
+
+    public void doIt(Object[] oldMessages, Object[] newMessages) {
+        if (actions != null && actions.size() > 0) {
+            try {
+                Map<String, Object> data = getData(oldMessages, newMessages);
+                T resultFromDimex = getDimexResult(data);
+
+                for (DimexAction ac : actions) {
+                    try {
+                        ac.doIt(resultFromDimex, data);
+                    } catch (Exception e) {
+                        if (logger.isErrorEnabled()) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                if (logger.isErrorEnabled()) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    protected abstract Map<String, Object> getData(Object[] oldMessages, Object[] newMessages);
+
+    protected abstract String getUrl(Map<String, Object> data);
+
+    private T getDimexResult(Map<String, Object> data) throws Exception {
+        RestTemplate restTemplate = templateFactory.createInstance();
+        restTemplate.setRequestFactory(user, password);
+
+        T result = (T)restTemplate.postForObject(getUrl(data), data, this.tClass);
+        return result;
+    }
+}
